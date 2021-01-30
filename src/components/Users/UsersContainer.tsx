6e10b8719d026f01a28fce58236526a4aef7cf7b
Mark Kadiner <dasistmail765@gmail.com>
2021-01-30 22:23:18 +0300

    55(47)

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import {connect} from "react-redux";
 import {CombinedStateType, DispatchType} from "../../redux/store";
 import {Users} from "./Users";
-import {followAC, setUsersAC, unfollowAC, UserType} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setUsersAC, unfollowAC, UserType} from "../../redux/users-reducer";
 
 let mapStateToProps = (state: CombinedStateType) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
     }
 }
 
@@ -20,6 +23,9 @@ let mapDispatchToProps = (dispatch: DispatchType) => {
         },
         setUsers: (users: Array<UserType>) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (currentPage: number) => {
+            dispatch(setCurrentPageAC(currentPage))
         }
     }
 }
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,30 +1,37 @@
 export type UserType = {
+    name: string
     id: number
-    photoUrl: string
-    followed: boolean
-    fullName: string
     status: string
-    location: {
-        city: string
-        country: string
+    photos: {
+        small: string
+        large: string
     }
+    followed: boolean
 }
 
 export type UsersStateType = {
     users: Array<UserType>
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
 }
 
 export type UsersActionsType =
     ReturnType<typeof followAC>
     | ReturnType<typeof unfollowAC>
     | ReturnType<typeof setUsersAC>
+    | ReturnType<typeof setCurrentPageAC>
 
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 
 let initialState: UsersStateType = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 19,
+    currentPage: 1
 }
 
 export const usersReducer = (state: UsersStateType = initialState, action: UsersActionsType) => {
@@ -44,6 +51,11 @@ export const usersReducer = (state: UsersStateType = initialState, action: Users
                 ...state,
                 users: [...state.users, ...action.users]
             }
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.currentPage
+            }
         default:
             return state
     }
@@ -52,4 +64,5 @@ export const usersReducer = (state: UsersStateType = initialState, action: Users
 export const followAC = (userId: number) => ({type: FOLLOW, userId} as const)
 export const unfollowAC = (userId: number) => ({type: UNFOLLOW, userId} as const)
 export const setUsersAC = (users: Array<UserType>) => ({type: SET_USERS, users} as const)
+export const setCurrentPageAC = (currentPage: number) => ({type: SET_CURRENT_PAGE, currentPage} as const)
 
